1dfe0ce2f3568fec42f5e1fcc3a7dd749b0f9324
Whymess <Jonathan.lev.messing@gmail.com>
2016-07-22 14:54:09 -0400

    Fixed order

--- src/main/resources/db/migration/faker.rb
+++ src/main/resources/db/migration/faker.rb
@@ -1,38 +1,47 @@
 require 'faker'
 require 'pry'
 
-class Products 
+class Product 
 	attr_accessor :name, :description,:chargeCard, :code,:price
 
-   def initialize(name, description, chargeCard, code,price)
+   def initialize(name,description,chargeCard,code, price)
 		@name = name
-		@code =  code
+    @description = description
+    @chargeCard = chargeCard
+    @code = code 
 		@price = price
-		@description = description
-		@chargeCard = chargeCard
    end 
 
     def self.createSQL(products,x)
 
       arrayOfSqlStatements = Array.new
       x.times do |x|
-         arrayOfSqlStatements << "INSERT INTO products (name, code, price, description,chargeCard) VALUES ('#{products[x].name}' , '#{products[x].description}', '#{products[x].chargeCard}', #{products[x].code}, #{products[x].price});"
+         arrayOfSqlStatements << "INSERT INTO products (name, code, price, description,chargeCard) VALUES ('#{products[x].name}','#{products[x].description}','#{products[x].chargeCard}', #{products[x].code}, #{products[x].price});" 
+
+
+  
+
+
+
+
       File.open('V2__insert_products.sql', 'w') {|f| f.write arrayOfSqlStatements.join("\n")}
       end 
     puts "Done, writing to file. "
   end 
     	
-
    def self.makeProduct(x)
    	products = Array.new
-   	x.times do |n|
-   		products.push(Products.new(Faker::Commerce.product_name, Faker::Code.isbn, Faker::Commerce.price, Faker::Lorem.words(2),Faker::Business.credit_card_type))
-   	end 
+    x.times do 
+    products.push(Product.new(Faker::Commerce.product_name, Faker::Lorem.words(2),Faker::Business.credit_card_type, Faker::Code.isbn, Faker::Commerce.price))
+
+   end 
+   
+
    		createSQL(products, x)
    end 
 
 
-   Products.makeProduct(100)
+   Product.makeProduct(3)
 
 
 
