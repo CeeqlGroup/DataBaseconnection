7c585678bc29c1ebeddd2a8f3e9762935c3c6c26
Whymess <Jonathan.lev.messing@gmail.com>
2016-07-22 13:56:15 -0400

    formatting complete

--- src/main/resources/db/migration/faker.rb
+++ src/main/resources/db/migration/faker.rb
@@ -10,35 +10,25 @@ class Products
 		@price = price
 		@description = description
 		@chargeCard = chargeCard
-
    end 
 
     def self.createSQL(products,x)
 
       arrayOfSqlStatements = Array.new
-
       x.times do |x|
          arrayOfSqlStatements << "INSERT INTO products (name, code, price, description,chargeCard) VALUES ('#{products[x].name}' , '#{products[x].description}', '#{products[x].chargeCard}', #{products[x].code}, #{products[x].price});"
-
-
       File.open('SqlStatements.sql', 'w') {|f| f.write arrayOfSqlStatements.join("\n")}
-
+      end 
+    puts "Writing to file....."
   end 
-  
-  puts "Writing to file....."
-
- end 
     	
  
 
-   def self.makeProduct(x) 
+   def self.makeProduct(x)
+
    	products = Array.new
-
-   
-
    	x.times do |n|
    		products.push(Products.new(Faker::Commerce.product_name,Faker::Lorem.words(2),Faker::Business.credit_card_type, Faker::Code.isbn,Faker::Commerce.price))
-
    	end 
    		createSQL(products, x)
    end 
